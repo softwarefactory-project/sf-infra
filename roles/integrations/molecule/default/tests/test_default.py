3ffac92d0f8dbf28026d01ea103854078eca506b
Matthieu Huin <mhuin@redhat.com>
2020-02-18 16:06:14 +0100

    sf: add integrations node
    The integrations node runs firehooks so that we keep our hooks on Taiga.
    
    Change-Id: I3cf443ff43d44d1b9a8735df5cdf9ca0da05b33c
    Related-To: TG-3240

---
+++ roles/integrations/molecule/default/tests/test_default.py
@@ -0,0 +1,69 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+test_cfg = """
+logging:
+  level: DEBUG
+broker:
+  url: softwarefactory-project.io
+  port: 1883
+projects:
+  - DLRN:
+    TaigaIO:
+      project: morucci-software-factory
+      auth:
+        user: sfbot0
+        password: xxxxx
+  - config:
+    TaigaIO:
+      project: morucci-software-factory
+      auth:
+        user: sfbot0
+        password: xxxxx
+  - software-factory:
+    TaigaIO:
+      project: morucci-software-factory
+      auth:
+        user: sfbot0
+        password: xxxxx
+  - repoxplorer:
+    TaigaIO:
+      project: morucci-software-factory
+      auth:
+        user: sfbot0
+        password: xxxxx
+  - logreduce:
+    TaigaIO:
+      project: morucci-software-factory
+      auth:
+        user: sfbot0
+        password: xxxxx
+  - rpms:
+    TaigaIO:
+      project: morucci-software-factory
+      auth:
+        user: sfbot0
+        password: xxxxx"""
+
+
+def test_defaults_config(host):
+    f = host.file('/etc/firehooks/default.yaml')
+
+    assert f.exists
+    assert f.user == 'firehook'
+    assert f.group == 'firehooks'
+
+    default = f.content_string
+    assert default == test_cfg
+
+
+def test_service_running(host):
+    f = host.service('firehooks')
+    assert f.is_enabled
+    assert f.is_running
