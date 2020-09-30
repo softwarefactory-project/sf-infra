92474b995976abf4364431080224c703f2392f4f
Javier Pena <jpena@redhat.com>
2020-09-30 11:54:21 +0200

    Fix afs-monitoring role
    - Change URL for the OpenDev graphite server to
      https://graphite.opendev.org
    - Use curl -k to skip SSL validation issues
    
    Change-Id: Ia1d59b3bc0de33f4dbc19402e3e50d9190228561

--- roles/afs-monitoring/files/get-afs-mirror-state.sh
+++ roles/afs-monitoring/files/get-afs-mirror-state.sh
@@ -2,6 +2,6 @@
 
 URL="${1}"
 
-TIMESTAMP=$(curl $URL 2>/dev/null | jq '.[0].datapoints[0][0]')
+TIMESTAMP=$(curl -k $URL 2>/dev/null | jq '.[0].datapoints[0][0]')
 [ -z $TIMESTAMP ] && echo 0 && exit 1
 echo $TIMESTAMP
