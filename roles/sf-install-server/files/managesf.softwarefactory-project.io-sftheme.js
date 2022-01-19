e198d5d42451d138b6b4147e6faf4d9f1ca79a00
Fabien Boucher <fboucher@redhat.com>
2022-01-19 09:52:01 +0000

    Since Gerrit 3.4 theme plugins must be JS
    See https://review.opendev.org/c/opendev/system-config/+/816618
    
    Change-Id: I544641fd45c6848114de0bf464fe7c35466dacc8

---
+++ roles/sf-install-server/files/managesf.softwarefactory-project.io-sftheme.js
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright (C) 2021 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+const openDevThemeStyle = document.createElement('dom-module');
+openDevThemeStyle.innerHTML = `
+       <template>
+         <style>
+           html {
+             --header-icon: url("https://softwarefactory-project.io/static/gerrit-logo.png");
+             --header-background-color: var(--background-color-primary);
+             --header-icon-size: 31px;
+           }
+         </style>
+       </template>
+ `;
+openDevThemeStyle.register('opendev-site-theme');
+Gerrit.install(plugin => {
+  plugin.registerStyleModule('app-theme', 'opendev-site-theme');
+});
