939f4e3fe6f4f860ea4dede22706298e159cbc0e
Daniel Pawlik <dpawlik@redhat.com>
2023-10-17 14:20:21 +0200

    Add feature to extracted CRC images that will add ssh keys from magic IP
    The extracted CRC image is based on CoreOS, which does not have
    cloud-init or ignition installed. It makes a problem to inject
    additional ssh pub keys if available.
    This commit is taking available pub keys from 169.254.169.254 address
    and add it to the authorized_keys via dedicated service, that is
    starting right after boot and before kubelet.
    Also added support for config-drive, when it is set during instance
    creation. The basic user_data that would be parsed looks like:
    
        #cloud-config
        password: core
        ssh_authorized_keys:
          - ssh-rsa AAAAB(...)Jh0CpE+1xQ4ow==
          - ssh-ed25519 AAAAC3(...)3dsAaY9
          - ecdsa-sha2-nistp256 AAAAE2VjZ(...)uq5QC51C5+WWk=
          - ssh-dss AAAyiM6qiF(...)iG8Wp4O73VUu2bg== someuser@host
    
    Change-Id: I24ec2dd85f6630406e216ebfe60a71488dd36cbd

--- roles/next-gen/extracted-crc/files/configure-pre-crc.sh
+++ roles/next-gen/extracted-crc/files/configure-pre-crc.sh
@@ -4,6 +4,10 @@ HOST_DEF_INT=$(ip -br -4 a sh | grep $HOST_IP | awk '{print $1}')
 ALL_DNS=$(nmcli -g IP4.DNS connection show $(nmcli connection show | grep $HOST_DEF_INT | awk -F '  ' '{print $2}'))
 DNS1=$(echo $ALL_DNS| cut -f1 -d'|')
 DNS2=$(echo $ALL_DNS| cut -f2 -d'|')
+USER_DIR=${USER_DIR:-$HOME}
+CLOUD_INIT_KEYS_URL=${CLOUD_INIT_KEYS_URL:-'http://169.254.169.254/1.0/meta-data/public-keys'}
+USERDATA_DIR=${USERDATA_DIR:-'/tmp/openstack-config-drive'}
+USERDATA_KEY_DIR=${USERDATA_KEY_DIR:-'/tmp/ssh-pub-keys'}
 
 if [ -z $DNS1 ]; then
     echo "DNS1 is empty. Setting 1.1.1.1"
@@ -41,3 +45,44 @@ dns=none
 EOL
 
 sudo systemctl reload NetworkManager
+
+# inject alternative SSH keys
+mkdir -p "$USERDATA_KEY_DIR" && cd "$USERDATA_KEY_DIR"
+
+# that will take ssh keys when openstack server create is spawned with:
+# --use-config-drive (--config-drive true)
+# Example of userdata:
+# #cloud-config
+# password: core
+# ssh_authorized_keys:
+#   - ssh-rsa AAAAB(...)Jh0CpE+1xQ4ow==
+#   - ssh-ed25519 AAAAC3(...)3dsAaY9
+#   - ecdsa-sha2-nistp256 AAAAE2VjZ(...)uq5QC51C5+WWk=
+#   - ssh-dss AAAyiM6qiF(...)iG8Wp4O73VUu2bg== someuser@host
+
+CDROM=$(lsblk -fp | grep iso9660 | awk '{print $1}')
+if [ -n "$CDROM" ]; then
+    mkdir -p "$USERDATA_DIR"
+    sudo mount "$CDROM" "$USERDATA_DIR"
+
+    if [ -f "$USERDATA_DIR/openstack/latest/user_data" ]; then
+        grep -E "^\s*- (ssh-rsa|ssh-ed25519|ecdsa-sha2-nistp256|ssh-dss)" "$USERDATA_DIR/openstack/latest/user_data" | sed 's/^\s*-\s*//' > "$USERDATA_KEY_DIR/userdata"
+        if ssh-keygen -l -f "$USERDATA_KEY_DIR/userdata"; then
+            cat "$USERDATA_KEY_DIR/userdata" | tee -a "${USER_DIR}/.ssh/authorized_keys"
+        fi
+    fi
+fi
+
+# it will use key from: openstack server create --key-name <mykey>
+CLOUD_INIT_KEYS=$(curl --connect-timeout 10 -SL "$CLOUD_INIT_KEYS_URL")
+if [ -n "$CLOUD_INIT_KEYS" ]; then
+    for k in $CLOUD_INIT_KEYS;
+    do
+        AVAILABLE_KEY=$(echo "$k" | cut -f1 -d"=")
+        curl -LO "$CLOUD_INIT_KEYS/$AVAILABLE_KEY/openssh-key"
+
+        if ssh-keygen -l -f openssh-key; then
+            cat openssh-key | tee -a "${USER_DIR}/.ssh/authorized_keys"
+        fi
+    done
+fi
