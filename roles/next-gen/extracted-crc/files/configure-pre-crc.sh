64718e7e1701cb74355e0ff0c6db8292b50e0629
Daniel Pawlik <dpawlik@redhat.com>
2024-10-14 10:15:05 +0200

    Enable configure dnsmasq for crc extracted image
    The image needs to include enabling service that is responsible to
    rewrite the dnsmasq configuration to host ip address.
    That is because when the VM is starting and the nodepool is waiting
    for other VMs to start, the CRC is making a lot request to resolve
    api-int.crc.testing domain, which is normally rejected by the DNS
    from our cloud provider. To imagine the scale of the issue, there is
    over 20 milion requests done in 1 hour.
    Similar change is also done in the CI job [1], but it is done too late.
    
    [1] https://github.com/openstack-k8s-operators/ci-framework/blob/57ef3f1382d0f4d6d3a25e514c6baa18fa092e06/roles/reproducer/tasks/configure_crc.yml#L31
    
    Change-Id: Iebab70aae519844b8b219bc8eb59b3969848c88a

--- roles/next-gen/extracted-crc/files/configure-pre-crc.sh
+++ roles/next-gen/extracted-crc/files/configure-pre-crc.sh
@@ -2,23 +2,9 @@
 
 set -x
 
+# NOTE:(dpawlik) Added a parameter to stop making a DNS request storm
+# for getting api-int.crc.testing domain.
 HOST_IP=$(ip route get 1.2.3.4 | awk '{print $7}' | head -n1)
-ALL_DNS=$(nmcli dev show | grep 'IP4.DNS'| awk '{print $2}')
-DNS1=$(echo $ALL_DNS | sed -n '1 p')
-DNS2=$(echo $ALL_DNS | sed -n '2 p')
-
-if [ -z $DNS1 ]; then
-    echo "DNS1 is empty. Setting 1.1.1.1"
-    DNS1='1.1.1.1'
-fi
-if [ -z $DNS2 ]; then
-    echo "DNS2 is empty. Setting 8.8.8.8"
-    DNS2='8.8.8.8'
-fi
-
-if [ "$DNS1" == "$DNS2" ]; then
-    DNS2='1.1.1.1'
-fi
 
 if [ -f /var/srv/dnsmasq.conf ]; then
     sed -i "s/192.168.130.11/$HOST_IP/g" /var/srv/dnsmasq.conf
@@ -26,28 +12,6 @@ fi
 
 if [ -f /etc/dnsmasq.d/crc-dnsmasq.conf ]; then
     sed -i "s/192.168.130.11/$HOST_IP/g" /etc/dnsmasq.d/crc-dnsmasq.conf
-
-    if grep -qv bind-interfaces /etc/dnsmasq.d/crc-dnsmasq.conf; then
-cat << EOL | sudo tee -a /etc/dnsmasq.d/crc-dnsmasq.conf
-# Add missing params
-bind-interfaces
-server=$DNS1
-server=$DNS2
-EOL
-    systemctl restart dnsmasq
-    fi
 fi
 
-cat << EOL | sudo tee /etc/resolv.conf
-nameserver $HOST_IP
-nameserver $DNS1
-nameserver $DNS2
-EOL
-
-# stop overwriting /etc/resolv.conf after reboot
-cat << EOL | sudo tee /etc/NetworkManager/conf.d/00-custom-crc.conf
-[main]
-dns=none
-EOL
-
-sudo systemctl reload NetworkManager
+systemctl restart dnsmasq
