7092247fbed531a8cc1eff026bae32a1cb5572a0
Tristan Cacqueray <tdecacqu@redhat.com>
2025-03-18 18:59:44 +0000

    Add full job-output decoder
    This change replace the hardcoded decoder with a proper JobOutput module.
    
    Change-Id: I134c3fc8c52af9d9e87ee8624e21be4c9c192f53

---
+++ tools/correlate-build-failure/JobOutput.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+-- | This module contains the data model for job-output.json
+module JobOutput where
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import GHC.Generics (Generic)
+
+type JobOutput = [JobPlaybook]
+type Hostname = Text
+
+data JobPlaybook = JobPlaybook
+    { phase :: Text
+    , playbook :: Text
+    , plays :: [JobPlay]
+    , trusted :: Bool
+    }
+    deriving (Show, Generic)
+instance FromJSON JobPlaybook
+instance ToJSON JobPlaybook
+
+newtype JobPlay = JobPlay {tasks :: [JobTask]} deriving (Show, Generic)
+instance FromJSON JobPlay
+instance ToJSON JobPlay
+
+data JobTask = JobTask
+    { hosts :: Map Hostname TaskResult
+    , task :: TaskInfo
+    , role :: Maybe RoleInfo
+    }
+    deriving (Show, Generic)
+instance FromJSON JobTask
+instance ToJSON JobTask
+
+data RoleInfo = RoleInfo
+    { name :: Text
+    , path :: Text
+    }
+    deriving (Show, Generic)
+instance FromJSON RoleInfo
+instance ToJSON RoleInfo
+
+newtype TaskInfo = TaskInfo {name :: Text} deriving (Show, Generic)
+instance FromJSON TaskInfo
+instance ToJSON TaskInfo
+
+data TaskResult = TaskResult
+    { action :: Text
+    , changed :: Bool
+    , failed :: Maybe Bool
+    , msg :: Maybe Text
+    , stdout :: Maybe Text
+    }
+    deriving (Show, Generic)
+instance FromJSON TaskResult
+instance ToJSON TaskResult
